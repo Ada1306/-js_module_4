6762386f543da91fecb9f3fba534ba6a474c69bd
Ada1306 <149617245+Ada1306@users.noreply.github.com>
2024-05-31 23:57:20 +0200

    Add js_module_4

--- js_module_4/script-21.js
+++ js_module_4/script-21.js
@@ -1,3 +1,6 @@
-function checkAgeLogicalOr(age) {
+function checkAge(age) {
     return age > 18  || confirm('Батьки дозволили?');
   }
+
+console.log(checkAge(20)); // Виведе: true
+console.log(checkAge(15)); // Виведе: результат confirm('Батьки дозволили?')
